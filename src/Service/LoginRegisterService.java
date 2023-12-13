17dbd36b77e0c3c85ede4bb2d159d2b796b286e6
Agungsusilo2 <105635993+Agungsusilo2@users.noreply.github.com>
2023-12-13 09:51:16 +0700

    LoginRegisterService.java

--- src/Service/LoginRegisterService.java
+++ src/Service/LoginRegisterService.java
@@ -3,9 +3,7 @@ package Service;
 import Entity.LoginRegister;
 
 public interface LoginRegisterService {
-    void showUserPassword();
     public void registerUser(LoginRegister loginRegister);
-
     boolean loginService(String username, String password);
 
 }
